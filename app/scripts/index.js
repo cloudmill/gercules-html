0556dc63c7834a91b8f8d8735feefd2da6d9af5c
szatolokin <staszatolokin28@gmail.com>
2020-09-02 19:01:17 +0300

    [UPDATE] Academy Desktop

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import App from "./main.js";
 
+
 $(document).ready(function() {
   let app = new App();
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,29 @@
 import $ from "jquery";
+import Swiper from "swiper";
+import select2 from "select2";
 
 //Tools
 import Listener from './tools/listener';
 
 export default class App {
-  constructor(){
+  constructor() {
     window.globalListener = new Listener();
+
+    const trainersSlider = new Swiper(".trainers .slides", {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 50,
+      allowTouchMove: false,
+      speed: 500,
+
+      navigation: {
+        nextEl: '.trainers .next',
+        prevEl: '.trainers .prev',
+      },
+    });
+
+    $(".calendar #location").select2({
+      minimumResultsForSearch: -1
+    });
   }
 };
