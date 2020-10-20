774ecbe9cb2c3ff4dedfba17f96bb1ca984b964b
szatolokin <staszatolokin28@gmail.com>
2020-10-20 18:58:48 +0300

    [UPDATE] start send-form, text-input, area-input, select-input bem-blocks

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,49 @@
 import $ from "jquery";
 import App from "./main.js";
 
+// NEW
+import "select2";
+// END NEW
+
 $(document).ready(function () {
   let app = new App();
   window.app = app;
+
+  // NEW
+  selectInputs();
+  // END NEW
 });
+
+// NEW
+function selectInputs() {
+  const selectInputs = $(".select-input__select");
+
+  selectInputs.each(function (index, element) {
+    const selectInput = $(element);
+    const selectInputOptions = {}
+
+    // search line
+    if (selectInput.data("no-search") !== undefined) {
+      selectInputOptions.minimumResultsForSearch = -1;
+    }
+
+    // width
+    selectInputOptions.width = selectInput.data("width");
+
+    // theme
+    selectInputOptions.theme = selectInput.data("theme");
+
+    // placeholder
+    const selectInputPlaceholder = selectInput.data("placeholder");
+    if (selectInputPlaceholder) {
+      selectInputOptions.placeholder = selectInputPlaceholder;
+    }
+
+    // dropdown place
+    const dropdownPlace = selectInput.closest(".select-input");
+    selectInputOptions.dropdownParent = dropdownPlace;
+
+    selectInput.select2(selectInputOptions);
+  });
+}
+// END NEW
