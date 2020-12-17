fd95581f4d519e0a839f154dc25283d36100dcae
CloudMill Digital Production <34512874+cloudmill@users.noreply.github.com>
2020-12-17 21:06:58 +0300

    js map script update

--- app/scripts/components/manager_maps.js
+++ app/scripts/components/manager_maps.js
@@ -28,13 +28,56 @@ export default class Manager_maps {
       //search: true,
     });
     $('.whereBuy-location select').change((e)=>{
-      map.update();
+      let city = $(".whereBuy-location select").val();
+      
+      $.ajax({
+        url: "/local/templates/s1/include/ajax/wherebuy/getLocationList.php",
+        type: "POST", //метод отправки
+        dataType: "html", //формат данных
+        data: {"city": city},
+        success: function (data) {
+          var location = $(data).find(".map-data");
+          var markets = $(data).find('.markets-wrapper');
+          $(".map-data").remove();
+          $(".whereBuy-map").append(location);
+          $(".markets-wrapper").remove();
+          $(".buy-markets-list").append(markets);
+          $('.buy-markets-type button').removeClass('active');
+          $('#all-button').addClass('active');
+          map.update();
+          console.log(city);
+        },
+        error: function (response) {
+          console.log("Ошибка обновления города!");
+        }
+      });
     })
     $('.buy-markets-type button').click((e)=>{
       if(!$(e.target).is('.active')){
         $('.buy-markets-type button').removeClass('active')
         $(e.target).addClass('active');
-        map.update();
+        let type = $(e.target).attr("data-key");
+        let city = $(".whereBuy-location select").val();
+
+        $.ajax({
+          url: "/local/templates/s1/include/ajax/wherebuy/getLocationList.php",
+          type: "POST", //метод отправки
+          dataType: "html", //формат данных
+          data: {"city": city, "type": type},
+          success: function (data) {
+            var location = $(data).find(".map-data");
+            var markets = $(data).find('.markets-wrapper');
+            $(".map-data").remove();
+            $(".whereBuy-map").append(location);
+            $(".markets-wrapper").remove();
+            $(".buy-markets-list").append(markets);
+            map.update();
+            console.log(type);
+          },
+          error: function (response) {
+            console.log("Ошибка обновления города!");
+          }
+        });
       }
     })
   }
